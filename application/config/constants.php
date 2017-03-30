9243ce57e8ca9a639d29b7db768763a54d0ce101
ryno888 <ryno888@gmail.com>
2017-03-30 16:13:17 +0200

    Setup

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,7 +84,7 @@ defined('EXIT_DATABASE')        OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')       OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')       OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-include_once BASEPATH."..\\application\\setup.php";
+include_once BASEPATH."../application/setup.php";
 
 //directories
 defined('DIR_APPLICATION')      OR define('DIR_APPLICATION', APPPATH); // no errors
