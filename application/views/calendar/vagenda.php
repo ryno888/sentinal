37c0f8d1380ad71c097cc0c40ecfc81ac25e8b79
ryno888 <ryno888@gmail.com>
2017-04-05 08:31:27 +0200

    Google Cloud

--- application/views/calendar/vagenda.php
+++ application/views/calendar/vagenda.php
@@ -8,15 +8,29 @@
 
 $client = new Google_Client([
     'application_name' => CI_NAME,
-    'client_id' => '531432974188-m04j7udtnli16fe8fu53qq9j0c698r21.apps.googleusercontent.com',
-    'client_secret' => 'C4IaT5pQQmnMypo7oNEwosBF',
-    'developer_key' => 'AIzaSyDxehi-L5eBqwzEH74Z30kRuwLoqeXX9QE',
+    'client_id' => '531432974188-u3iujp6045b1k0a2ll82egj3p1ts1uc0.apps.googleusercontent.com',
+    'client_secret' => 'Cno6s2f5jTG5CYzwtCwC5yI-',
+    'developer_key' => '1e50a8651772bf3b384dfb350e7509d05df11e20',
 ]);
 
 $client->setHttpClient(new \GuzzleHttp\Client(array(
     'verify' => false,
 )));
 
-$service = new Google_Service_Calendar($client);
-$calendar = $service->calendars->get('primary');
-console($calendar->getSummary());
+//$service = new Google_Service_Calendar($client);
+//$calendar = $service->calendars->get('primary');
+//console($calendar->getSummary());
+
+//$client = new Google_Client();
+//$client->setAuthConfig('client_secrets.json');
+//$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+  $drive = new Google_Service_Drive($client);
+  $files = $drive->files->listFiles(array())->getItems();
+  echo json_encode($files);
+} else {
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
